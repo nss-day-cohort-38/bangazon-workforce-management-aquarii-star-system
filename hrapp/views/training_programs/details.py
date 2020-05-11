858c0e6f703ff163bb5c7a41879395cda741724f
keithrpotempa <keithrpotempa@gmail.com>
2020-05-11 13:03:55 -0500

    paused, working on delete

--- hrapp/views/training_programs/details.py
+++ hrapp/views/training_programs/details.py
@@ -27,6 +27,7 @@ def get_training_program(training_program_id):
         return db_cursor.fetchone()
 
 def training_program_details(request, training_program_id):
+    # Check if its a GET (showing details)
     if request.method == 'GET':
         training_program = get_training_program(training_program_id)
 
@@ -36,3 +37,22 @@ def training_program_details(request, training_program_id):
         }
 
         return render(request, template, context)
+    
+    # Check if its a POST (edit or delete)
+    elif request.method == 'POST':
+        form_data = request.POST
+        
+        # Check if this POST is for deleting
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM hrapp_trainingprogram
+                WHERE id = ?
+                """, (training_program_id,))
+
+            return redirect(reverse('hrapp:training_program'))
