7fd6985f4096647abddace8aa7dd8003cbe84c75
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-11 11:02:34 -0500

    employees show up on employee list page, working on having them show up on the details page

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -2,9 +2,9 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from hrapp.models import Department
+from hrapp.models import Department, Employee
 from ..connection import Connection
-
+from .list import create_employee_list
 
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
@@ -15,8 +15,11 @@ def get_department(department_id):
         SELECT
             d.id,
             d.dept_name,
-            d.budget
+            d.budget,
+            e.first_name,
+            e.last_name
         FROM hrapp_department d
+        JOIN hrapp_employee e ON d.id = e.department_id
         WHERE d.id = ?
         """, (department_id,))
 
@@ -27,9 +30,15 @@ def department_details(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
 
+        # department_employees = []
+
+        # for employee in department:
+        #     department_employees.append(employee)
+
         template = 'departments/detail.html'
         context = {
-            'department': department
+            'department': department,
+            # 'employees': department_employees
         }
 
         return render(request, template, context)
--- hrapp/views/departments/list.py
+++ hrapp/views/departments/list.py
@@ -4,59 +4,49 @@ from hrapp.models import Department, Employee
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
 
-def create_employee_list(cursor, row):
-    _row = sqlite3.Row(cursor, row)
-
-    employee = Employee()
-    library.id = _row["id"]
-    library.title = _row["title"]
-    library.address = _row["address"]
-
-    # Note: You are adding a blank books list to the library object
-    # This list will be populated later (see below)
-    library.books = []
-
-    book = Book()
-    book.id = _row["book_id"]
-    book.title = _row["book_title"]
-    book.author = _row["author"]
-    book.isbn = _row["isbn"]
-    book.year_published = _row["year_published"]
-
-    # Return a tuple containing the library and the
-    # book built from the data in the current row of
-    # the data set
-    return (library, book,)
-
 @login_required
 def department_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
+            conn.row_factory = create_employee_list
             db_cursor = conn.cursor()
 
             db_cursor.execute("""
-            select
+            SELECT
                 d.id,
                 d.dept_name,
-                d.budget
-            from hrapp_department d
+                d.budget,
+                e.first_name,
+                e.last_name
+            FROM hrapp_department d
+            JOIN hrapp_employee e ON d.id = e.department_id
             """)
 
-            all_departments = []
-            dataset = db_cursor.fetchall()
+            # all_departments = []
+            # dataset = db_cursor.fetchall()
 
-            for row in dataset:
-                department = Department()
-                department.id = row['id']
-                department.dept_name = row['dept_name']
-                department.budget = row['budget']
+            departments = db_cursor.fetchall()
 
-                all_departments.append(department)
+            department_groups = {}
+
+            # for row in dataset:
+            #     department = Department()
+            #     department.id = row['id']
+            #     department.dept_name = row['dept_name']
+            #     department.budget = row['budget']
+
+            #     all_departments.append(department)
+
+            for (department, employee) in departments:
+                if department.id not in department_groups:
+                    department_groups[department.id] = department
+                    department_groups[department.id].employees.append(employee)
+                else:
+                    department_groups[department.id].employees.append(employee)
 
         template = 'departments/list.html'
         context = {
-            'all_departments': all_departments
+            'all_departments': department_groups.values()
         }
 
         return render(request, template, context)
@@ -77,3 +67,20 @@ def department_list(request):
             (form_data['dept_name'], form_data['budget']))
 
         return redirect(reverse('hrapp:departments'))
+
+def create_employee_list(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    department = Department()
+    department.id = _row["id"]
+    department.dept_name = _row["dept_name"]
+    department.budget = _row["budget"]
+
+    department.employees = []
+
+    employee = Employee()
+    employee.id = _row["id"]
+    employee.first_name = _row["first_name"]
+    employee.last_name = _row["last_name"]
+
+    return (department, employee,)
