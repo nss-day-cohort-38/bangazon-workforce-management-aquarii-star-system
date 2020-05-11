7805c63ded653ceafdd8627621108f540647ed4a
MReeds <mattlreeds@gmail.com>
2020-05-11 13:36:27 -0500

    added delete functionality

--- hrapp/views/computers/details.py
+++ hrapp/views/computers/details.py
@@ -33,3 +33,20 @@ def computer_details(request, computer_id):
         }
         
         return render(request, template, context)
+    
+    if request.method == 'POST':
+        form_data = request.POST
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM hrapp_computer
+                WHERE id = ?
+                """, (computer_id,))
+
+            return redirect(reverse('hrapp:computers'))
