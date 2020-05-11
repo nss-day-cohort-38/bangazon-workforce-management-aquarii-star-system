d79c160f68dbf56670065c4f8d67a3e4c01d5683
MReeds <mattlreeds@gmail.com>
2020-05-11 15:49:09 -0500

    finished ticket

--- hrapp/views/computers/details.py
+++ hrapp/views/computers/details.py
@@ -5,6 +5,24 @@ from hrapp.views.connection import Connection
 from hrapp.models.modelfactory import model_factory
 from hrapp.models.computer import Computer
 
+def get_employee_computer(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Computer)
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute('''
+        SELECT 
+            c.id,
+            c.make,
+            c.manufacturer
+        FROM hrapp_computer c 
+        LEFT JOIN hrapp_employeecomputer e
+        WHERE e.computer_id = ?;
+            ''', (computer_id,))
+         
+        return db_cursor.fetchall()
+
+    
 def get_computer(computer_id):
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = model_factory(Computer)
@@ -22,15 +40,21 @@ def get_computer(computer_id):
         """, (computer_id,))
         
         return db_cursor.fetchone()
-
+# Write function to determine whether can be deleted is true or false based on connected to employee
 def computer_details(request, computer_id):
     if request.method == 'GET':
         computer = get_computer(computer_id)
+        employee_computer = get_employee_computer(computer_id)
+        
+        canDelete = False
+
+        if len(employee_computer) is 0:
+            canDelete = True
         
         template = 'computers/detail.html'
         context = {
-            'computer': computer
-        }
+            'computer': computer,
+            'canDelete': canDelete}
         
         return render(request, template, context)
     
