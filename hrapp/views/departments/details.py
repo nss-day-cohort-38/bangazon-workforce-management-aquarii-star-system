d9ecbb3784fb1e050b730fac2ad1c5b3ff6d077d
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-11 15:57:36 -0500

    commiting for a pull request review

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from hrapp.models import Department, Employee
 from ..connection import Connection
 
-
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = sqlite3.Row
@@ -45,21 +44,21 @@ def department_details(request, department_id):
 
         return render(request, template, context)
 
-# def create_department(cursor, row):
-#     _row = sqlite3.Row(cursor, row)
+def create_department(cursor, row):
+    _row = sqlite3.Row(cursor, row)
 
-#     department = Department()
-#     department.id = _row["id"]
-#     department.dept_name = _row["dept_name"]
-#     department.budget = _row["budget"]
+    department = Department()
+    department.id = _row["id"]
+    department.dept_name = _row["dept_name"]
+    department.budget = _row["budget"]
 
-#     department.employees = []
+    department.employees = []
 
-#     employee = Employee()
-#     employee.id = _row["id"]
-#     employee.first_name = _row["first_name"]
-#     employee.last_name = _row["last_name"]
+    employee = Employee()
+    employee.id = _row["id"]
+    employee.first_name = _row["first_name"]
+    employee.last_name = _row["last_name"]
 
-#     return (department, employee,)
+    return (department, employee,)
 
 
