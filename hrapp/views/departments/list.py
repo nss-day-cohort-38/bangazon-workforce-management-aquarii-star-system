d967dce7e3baf1eb80dc6603ab730b447d82d508
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-11 09:36:56 -0500

    working on adding employee count to department list

--- hrapp/views/departments/list.py
+++ hrapp/views/departments/list.py
@@ -1,8 +1,32 @@
 import sqlite3
 from django.shortcuts import render, redirect, reverse
-from hrapp.models import Department
+from hrapp.models import Department, Employee
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
+
+def create_employee_list(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    employee = Employee()
+    library.id = _row["id"]
+    library.title = _row["title"]
+    library.address = _row["address"]
+
+    # Note: You are adding a blank books list to the library object
+    # This list will be populated later (see below)
+    library.books = []
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.title = _row["book_title"]
+    book.author = _row["author"]
+    book.isbn = _row["isbn"]
+    book.year_published = _row["year_published"]
+
+    # Return a tuple containing the library and the
+    # book built from the data in the current row of
+    # the data set
+    return (library, book,)
 
 @login_required
 def department_list(request):
