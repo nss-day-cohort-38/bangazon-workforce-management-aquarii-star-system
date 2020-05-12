66ccb60e8c0cc24d5fcaeec2b2ce06df216eb6f7
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-12 09:53:59 -0500

    working on getting employee list on department details page

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -5,20 +5,18 @@ from django.contrib.auth.decorators import login_required
 from hrapp.models import Department, Employee
 from ..connection import Connection
 
+
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = sqlite3.Row
+        conn.row_factory = create_department
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
         SELECT
             d.id,
             d.dept_name,
-            d.budget,
-            e.first_name,
-            e.last_name
+            d.budget
         FROM hrapp_department d
-        LEFT JOIN hrapp_employee e ON d.id = e.department_id
         WHERE d.id = ?
         """, (department_id,))
 
@@ -31,15 +29,9 @@ def department_details(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
 
-        # department_employees = []
-
-        # for employee in department:
-        #     department_employees.append(employee)
-
         template = 'departments/detail.html'
         context = {
             'department': department
-            # 'employees': department_employees
         }
 
         return render(request, template, context)
@@ -52,13 +44,6 @@ def create_department(cursor, row):
     department.dept_name = _row["dept_name"]
     department.budget = _row["budget"]
 
-    department.employees = []
-
-    employee = Employee()
-    employee.id = _row["id"]
-    employee.first_name = _row["first_name"]
-    employee.last_name = _row["last_name"]
-
-    return (department, employee,)
+    return department
 
 
