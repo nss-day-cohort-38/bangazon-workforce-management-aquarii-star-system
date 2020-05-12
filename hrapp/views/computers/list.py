1396925747b40cbbbba2259e8acb7ec0ee4f86fd
MReeds <mattlreeds@gmail.com>
2020-05-12 15:43:07 -0500

    still working out the bugs

--- hrapp/views/computers/list.py
+++ hrapp/views/computers/list.py
@@ -44,11 +44,18 @@ def computer_list(request):
                     purchase_date,
                     manufacturer
             )
-            VALUES (?, ?, ?)
+            VALUES (?, ?, ?);
             """,
             (form_data['make'], form_data['purchase_date'],
                 form_data['manufacturer']))
             
+            new_computer_id = get_that_id(form_data['make'], form_data['purchase_date'],form_data['manufacturer'])
+            print("The computer id is:", new_computer_id)
+            # employee_and_computers()
+
+        return redirect(reverse('hrapp:computers'))
+            
+def employee_and_computers(computer_id, employee_id):
         with sqlite3.connect(Connection.db_path) as conn:
             db_cursor = conn.cursor()
             
@@ -58,8 +65,26 @@ def computer_list(request):
                         computer_id,
                         employee_id
                     )
-                VALUES (?, ?)
+                VALUES (?, ?);
             """, 
-            (form_data['computer_id'], form_data['employee_id']))
-
-        return redirect(reverse('hrapp:computers'))
+            (computer_id, employee_id))
+            
+def get_that_id(make, purchase_date, manufacturer):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Computer)
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute('''
+        SELECT
+            c.id,
+            c.make,
+            c.purchase_date,
+            c.manufacturer
+        FROM hrapp_computer c
+        WHERE c.make = ?
+        AND c.purchase_date = ?
+        AND c.manufacturer = ?;
+        ''', 
+        (make, purchase_date, manufacturer, ))
+        
+    return db_cursor.fetchall()
