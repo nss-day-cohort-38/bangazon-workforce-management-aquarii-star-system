d03abfcb032bd774786ec39d68e5d82c30c451b9
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-12 10:31:03 -0500

    employees now show up on department details page

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -29,9 +29,12 @@ def department_details(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
 
+        employees = get_employees(department_id)
+
         template = 'departments/detail.html'
         context = {
-            'department': department
+            'department': department,
+            'employees': employees
         }
 
         return render(request, template, context)
@@ -45,5 +48,28 @@ def create_department(cursor, row):
     department.budget = _row["budget"]
 
     return department
+
+def get_employees(department_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.first_name,
+            e.last_name,
+            e.start_date,
+            e.is_supervisor,
+            e.department_id
+        FROM hrapp_employee e
+        WHERE e.department_id = ?
+        """, (department_id,))
+
+        employees = db_cursor.fetchall()
+
+        return employees
+
+
 
 
