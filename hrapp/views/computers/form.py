a7cf0224e692114c3ac6b6b8440e48ab51eb4b6e
MReeds <mattlreeds@gmail.com>
2020-05-12 13:27:00 -0500

    still working on post

--- hrapp/views/computers/form.py
+++ hrapp/views/computers/form.py
@@ -3,7 +3,28 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from hrapp.models.modelfactory import model_factory
 from hrapp.views.connection import Connection
-from hrapp.models.employee import Employee
+from hrapp.models import Employee, Computer
+
+def get_employee_computer(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Computer)
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute('''
+        SELECT 
+            c.id AS computer_id,
+            c.make,
+            c.manufacturer,
+            ec.employee_id,
+            e.first_name,
+            e.last_name
+        FROM hrapp_computer c 
+        JOIN hrapp_employeecomputer ec
+        JOIN hrapp_employee e
+        WHERE ec.computer_id = c.id;
+            ''', (computer_id,))
+         
+        return db_cursor.fetchall()
 
 def get_employees():
     with sqlite3.connect(Connection.db_path) as conn:
@@ -22,8 +43,10 @@ def get_employees():
 def computer_form(request):
     if request.method == 'GET':
         employees = get_employees()
+        employee_computer = get_employee_computer()
         template = 'computers/form.html'
         context = {
-            'employees': employees
+            'employees': employees,
+            'employee_computer': employee_computer
         }
         return render(request, template, context)
