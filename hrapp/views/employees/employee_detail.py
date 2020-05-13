ae0d32480751feed7cd671960f96e486f1748c3b
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-13 15:03:42 -0500

    reformatted to use multiple sql queries

--- hrapp/views/employees/employee_detail.py
+++ hrapp/views/employees/employee_detail.py
@@ -4,43 +4,47 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Employee, Computer, TrainingProgram, Department
 from ..connection import Connection
+from datetime import date
 
+employee = Employee()
+employee.computers = []
+employee.training_programs = []
 
 def create_employee(cursor, row):
     _row = sqlite3.Row(cursor, row)
 
-    employee = Employee()
     employee.id = _row["employee_id"]
     employee.first_name = _row["first_name"]
-    employee.last_name = _row["last_name"]
-    employee.start_date = _row["start_date"]
+    employee.last_name= _row["last_name"]
+
+    return employee
+
+def create_employee_with_department(cursor, row):
+    _row = sqlite3.Row(cursor, row)
 
     department = Department()
     department.id = _row["dept_id"]
-    department.name = _row["dept_name"]
-    department.budget = _row["dept_budget"]
+    department.dept_name = _row["dept_name"]
     employee.department = department
 
-    computer = Computer()
-    computer.id = _row["comp_id"]
-    computer.manufacturer = _row["computer_manufacturer"]
-    computer.make = _row["computer_make"]
-    computer.purchase_date = _row["comp_purchase_date"]
-    computer.decommission_date = _row["comp_decommission_date"]
-    employee.computer = computer
+    return department
 
-    employee.training_programs = []
+def create_employee_computers(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    computer = Computer()
+    computer.manufacturer = _row["comp_manufacturer"]
+    computer.make = _row["comp_make"]
+    
+    return computer
+
+def create_employee_training_programs(cursor, row):
+    _row = sqlite3.Row(cursor, row)
 
     training_program = TrainingProgram()
-    training_program.id = _row["training_program_id"]
-    training_program.title = _row["training_program_title"]
-    training_program.description = _row["training_program_description"]
-    training_program.start_date = _row["training_program_start_date"]
-    training_program.end_date = _row["training_program_end_date"]
-    training_program.capacity = _row["training_program_capacity"]
+    training_program.title = _row["title"]
 
-    return (employee, department, computer, training_program)
-
+    return training_program
 
 def get_employee(employee_id):
     with sqlite3.connect(Connection.db_path) as conn:
@@ -51,57 +55,100 @@ def get_employee(employee_id):
         SELECT
             e.id AS employee_id,
             e.first_name,
-            e.last_name,
-            e.start_date,
-            d.id AS dept_id,
-            d.dept_name,
-            d.budget AS dept_budget,
-            c.id AS comp_id,
-            c.purchase_date AS comp_purchase_date,
-            c.decommission_date AS comp_decommission_date,
-            c.manufacturer AS computer_manufacturer,
-            c.make AS computer_make,
-            tp.id AS training_program_id,
-            tp.title AS training_program_title,
-            tp.description AS training_program_description,
-            tp.start_date AS training_program_start_date,
-            tp.end_date AS training_program_end_date,
-            tp.capacity AS training_program_capacity
+            e.last_name
         FROM
             hrapp_employee e
-            LEFT JOIN hrapp_department d ON d.id = e.department_id
-            LEFT JOIN hrapp_employeecomputer ec ON e.id = ec.employee_id
-            LEFT JOIN hrapp_computer c ON ec.computer_id = c.id
-            LEFT JOIN hrapp_employeetrainingprogram etp ON etp.employee_id = e.id
-            LEFT JOIN hrapp_trainingprogram tp ON etp.training_program_id = tp.id
         WHERE
             e.id = ?
         """, (employee_id,))
 
-    employee_data = db_cursor.fetchall()
+    data = db_cursor.fetchone()
+    
+    return data
 
-    employee_with_programs = {}
+def get_employee_training_programs(employee_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_employee_training_programs
+        db_cursor = conn.cursor()
 
-    for (employee, department, computer, training_program) in employee_data:
+        db_cursor.execute("""
+        SELECT
+            e.id AS employee_id,
+            tp.title
+        FROM
+            hrapp_employee e
+            JOIN hrapp_employeetrainingprogram etp ON etp.employee_id = e.id
+            LEFT JOIN hrapp_trainingprogram tp ON etp.training_program_id = tp.id
+        WHERE
+            e.id = ?;
+	""", (employee_id,))
+    
+    data = db_cursor.fetchall()
+    return data
 
-        if employee.id not in employee_with_programs:
-            employee_with_programs[employee.id] = employee
-            employee_with_programs[employee.id].training_programs.append(training_program)
+def get_employee_department(employee_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_employee_with_department
+        db_cursor = conn.cursor()
 
-        else:
-            employee_with_programs[employee.id].training_programs.append(training_program)
+        db_cursor.execute("""
+        SELECT
+            e.id AS employee_id,
+            d.id AS dept_id,
+            d.dept_name
+        FROM
+            hrapp_employee e
+            LEFT JOIN hrapp_department d ON d.id = e.department_id
+        WHERE
+            e.id = ?;
+        """, (employee_id,))
 
-    return employee_with_programs[employee_id]
+        data = db_cursor.fetchone()
 
+        return data
+
+def get_employee_computer(employee_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_employee_computers
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id AS employee_id,
+            c.manufacturer AS comp_manufacturer,
+            c.make AS comp_make
+        FROM
+            hrapp_employee e
+            JOIN hrapp_employeecomputer ec ON ec.employee_id = e.id
+            LEFT JOIN hrapp_computer c ON c.id = ec.computer_id
+        WHERE
+            e.id = ?;
+        """, (employee_id, ))
+
+        data = db_cursor.fetchone()
+
+        return data
 
 def employee_detail(request, employee_id):
     if request.method == 'GET':
         employee = get_employee(employee_id)
+        employee_department = get_employee_department(employee_id)
+        employee_computer = get_employee_computer(employee_id)
+        employee_training_programs = get_employee_training_programs(employee_id)
         template_name = 'employees/employee_details.html'
-        return render(request, template_name, {'employee': employee})
+        context = {
+          "employee": employee,
+          "department": employee_department,
+          "computer": employee_computer,
+          "training_programs": employee_training_programs
+        }
+
+        return render(request, template_name, context)
+
 
     elif request.method == 'POST':
         form_data = request.POST
+        get_employee_computer(employee_id)
 
         if (
             "actual_method" in form_data
@@ -120,13 +167,24 @@ def employee_detail(request, employee_id):
                     form_data['last_name'], form_data["department"], employee_id,
                 ))
 
-                db_cursor.execute("""
-                UPDATE hrapp_employeecomputer
-                SET computer_id = ?
-                WHERE employee_id = ?
-                """,
-                (
-                    form_data['computer'], employee_id,
-                ))
-
-            return redirect(reverse('hrapp:employees'))
+                if employee_computer is None:
+                    db_cursor.execute("""
+                    INSERT INTO hrapp_employeecomputer
+                    (employee_id, computer_id, assign_date, unassign_date)
+                    VALUES (?, ?, ?, ?)
+                    """,
+                    (
+                        employee_id, form_data['computer'], date.today(), None
+                    ))
+                
+                else:
+                    db_cursor.execute("""
+                    UPDATE hrapp_employeecomputer
+                    SET computer_id = ?
+                    WHERE employee_id = ?
+                    """,
+                    (
+                        form_data['computer'], employee_id,
+                    ))
+                
+                return redirect(reverse('hrapp:employees'))
