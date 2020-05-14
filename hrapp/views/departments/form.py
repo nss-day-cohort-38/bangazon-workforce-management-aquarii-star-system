99b6c4a92495781928164a16bb071e8aa8fd0b3b
MReeds <mattlreeds@gmail.com>
2020-05-14 10:27:58 -0500

    added user authentication for forms and pages

--- hrapp/views/departments/form.py
+++ hrapp/views/departments/form.py
@@ -2,10 +2,10 @@ import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Department
-from ..connection import Connection
-# from django.contrib.auth.decorators import login_required
+from hrapp.views.connection import Connection
+from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def department_form(request):
     if request.method == 'GET':
         template = 'departments/form.html'
